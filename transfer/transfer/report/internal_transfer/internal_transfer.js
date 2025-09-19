ea4a63b50e9ee196767e0a3a90039de86b443a06
awad <awd@hotmail.it>
2025-09-19 17:27:42 +0200

    feat:init internal transfer  script report

---
+++ transfer/transfer/report/internal_transfer/internal_transfer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, awad mohamed & atta almanan and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["internal transfer"] = {
+	"filters": [
+
+	]
+};
