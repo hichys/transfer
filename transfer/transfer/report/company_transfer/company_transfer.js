e4d82c888f0ca3647562deb89fde9adc82e6ba96
awad <awd@hotmail.it>
2025-09-19 04:23:14 +0200

    feat: add filters for sender and receiver companies in company transfer report

--- transfer/transfer/report/company_transfer/company_transfer.js
+++ transfer/transfer/report/company_transfer/company_transfer.js
@@ -1,8 +1,74 @@
-// Copyright (c) 2025, awad mohamed & atta almanan and contributors
-// For license information, please see license.txt
-
+ 
 frappe.query_reports["company transfer"] = {
 	"filters": [
+		{
+			fieldname: "from_company",
+			label: __("Sender Company"),
+			fieldtype: "Link",
+			options: "Customer",
+			get_query: function () {
+				const to_company = frappe.query_report.get_filter_value('to_company');
+				let filters = { "customer_type": "Company" };
+
+				if (to_company) {
+					filters["name"] = ["!=", to_company];
+				}
+
+				return { filters: filters };
+			}
+		},
+		{
+			fieldname: "to_company",
+			label: __("Receiver Company"),
+			fieldtype: "Link",
+			options: "Customer",
+			get_query: function () {
+				const from_company = frappe.query_report.get_filter_value('from_company');
+				let filters = { "customer_type": "Company" };
+
+				if (from_company) {
+					filters["name"] = ["!=", from_company];
+				}
+
+				return { filters: filters };
+			}
+		},
+		{
+			fieldname: "workflow_state",
+			label: __("Status"),
+			fieldtype: "Select",
+			options: [
+				'مستلمة',
+				'غير مستلمة',
+				'ملغية',
+				'غير مسجلة'
+			],
+			default: 'غير مستلمة',
+			reqd: 1
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1
+		},
+		{
+			fieldname: "reciver_or_sender",
+			label: __("filter by Reciver or Sender"),
+			fieldtype: "Select",
+			options: ['Reciver', 'Sender'],
+			default: "Sender",
+			reqd: 1
+		},
+
 
 	]
 };
