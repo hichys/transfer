f1a48176d726f6e84fc9013f065151e3befc253c
awad <awd@hotmail.it>
2025-09-19 03:59:29 +0200

    feat: add company transfer list view settings

---
+++ transfer/transfer/doctype/company_transfer/company_transfer_list.js
@@ -0,0 +1,6 @@
+frappe.listview_settings['company transfer'] = {
+    // Remove the default docstatus column from view
+    has_indicator_for_draft: true,
+    has_indicator_for_cancelled: true,
+    
+};
