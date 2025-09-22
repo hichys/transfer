a8ecdf029f3fc0437bb70ef5a571310aa6b2f5ff
awad <awd@hotmail.it>
2025-09-22 07:09:54 +0200

    feat: change internal transfer default indicator list view setting

---
+++ transfer/transfer/doctype/internal_transfer/internal_transfer_list.js
@@ -0,0 +1,6 @@
+frappe.listview_settings['Internal Transfer'] = {
+    // Remove the default docstatus column from view
+    has_indicator_for_draft: true,
+    has_indicator_for_cancelled: true,
+
+};
